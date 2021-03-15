3dc44ab36ae919236f64df36c7574c3bb5a53dcb
Samia Ferdous <samia.ferdous@icloud.com>
2021-03-14 20:03:55 -0400

    add conditional statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,11 +14,19 @@ var fight = function() {
     
     // Subtract the value of 'playAttack' from the value of enemyHealth' and use that to update the 'enemyHealth' variable.
         enemyHealth = enemyHealth - playerAttack;
-        
+
     // Log a resulting message to the console so we know that it worked.
         console.log(
         playerName + " attacked" + enemyName + ". " + enemyName + " now has" + enemyHealth + " health remaining."
         );
+
+    // Check enemy's health.
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
         playerHealth = playerHealth - enemyAttack;
@@ -27,6 +35,12 @@ var fight = function() {
         console.log(
         enemyName + " attacked" + playerName + ". " + playerName + " now has" + playerHealth + " health remaining."
         );
-};
+    
+    //check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else window.alert(playerName + " still has " + playerHealth + " health left.");
+}
 
 fight();
